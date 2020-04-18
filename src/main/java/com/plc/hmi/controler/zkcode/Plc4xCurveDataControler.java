cac9335d0dbeae8ea9b07cebf890f06f70dbd358
zhengkai <1157377822@qq.com>
2020-04-18 21:33:07 +0800

    提交

--- src/main/java/com/plc/hmi/controler/zkcode/Plc4xCurveDataControler.java
+++ src/main/java/com/plc/hmi/controler/zkcode/Plc4xCurveDataControler.java
@@ -1,10 +1,7 @@
-package com.plc.hmi.controler.zkcode;
+﻿package com.plc.hmi.controler.zkcode;
 import com.plc.hmi.dal.entity.PressureCurveEntity;
 import com.plc.hmi.dal.entity.plc.PlcEntity;
-import com.plc.hmi.service.plcService.Plc4xCurveDataService;
-import com.plc.hmi.service.plcService.Plc4xCurveStatusService;
-import com.plc.hmi.service.plcService.Plc4xEquipmentIoStatusService;
-import com.plc.hmi.service.plcService.Plc4xEquipmentStatusService;
+import com.plc.hmi.service.plcService.*;
 import io.netty.util.internal.RecyclableArrayList;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -109,9 +106,45 @@ public class Plc4xCurveDataControler {
     @GetMapping("/EquipmentIoStatus")
     /**
      * 获取设备io监控
+     *
+     * 页面中文LABEL: 输入_0 ,值变量名: input0 ,值类型 WORD
+     页面中文LABEL: 输入_1 ,值变量名: input1 ,值类型 WORD
+     页面中文LABEL: 输入_2 ,值变量名: input2 ,值类型 WORD
+     页面中文LABEL: 输入_3 ,值变量名: input3 ,值类型 WORD
+     页面中文LABEL: 输入_4 ,值变量名: input4 ,值类型 WORD
+     页面中文LABEL: 输出_0 ,值变量名: output0 ,值类型 WORD
+     页面中文LABEL: 输出_1 ,值变量名: output1 ,值类型 WORD
+     页面中文LABEL: 输出_2 ,值变量名: output2 ,值类型 WORD
+     页面中文LABEL: 输出_3 ,值变量名: output3 ,值类型 WORD
+     页面中文LABEL: 输出_4 ,值变量名: output4 ,值类型 WORD
      */
     List<PlcEntity> getEquipmentIo(){
-    return plc4xEquipmentIoStatusService.getDatas();
+        List<PlcEntity> datas = plc4xEquipmentIoStatusService.getDatas();
+        datas.stream().forEach(p-> {
+            if ("input0".equals(p.getName())) {
+                p.setName("输入_0");
+            } else if ("input1".equals(p.getName())) {
+                p.setName("输入_1");
+            } else if ("input2".equals(p.getName())) {
+                p.setName("输入_2");
+            } else if ("input3".equals(p.getName())) {
+                p.setName("输入_3");
+            } else if ("input4".equals(p.getName())) {
+                p.setName("输入_4");
+            } else if ("输出_0".equals(p.getName())) {
+                p.setName("output0");
+            } else if ("output1".equals(p.getName())) {
+                p.setName("输出_1");
+            } else if ("output2".equals(p.getName())) {
+                p.setName("输出_2");
+            } else if ("output3".equals(p.getName())) {
+                p.setName("输出_3");
+            } else if ("output4".equals(p.getName())) {
+                p.setName("输出_4");
+            }
+        });
+
+        return datas;
     }
     @GetMapping("/getCurveDatas")
     /**
@@ -119,5 +152,36 @@ public class Plc4xCurveDataControler {
      */
     List<PressureCurveEntity> getCurveDatas(){
     return plc4xCurveDataService.getCurveDatas();
+    }
+
+
+    Plc4xEquipmentOperationService plc4xEquipmentOperationService;
+
+    /**
+     *  获得安全监控
+     * @return
+     */
+    @GetMapping("/getOperation")
+    public List<PlcEntity> getDatas(){
+        List<PlcEntity> datas = plc4xEquipmentOperationService.getDatas();
+        datas.stream().forEach(p->{
+            if ("productNo".equals(p.getName())){
+                p.setName("零件号");
+            }else if ("indenterChoice".equals(p.getName())){
+                p.setName("压头选择");
+            }else if ("choicePositive".equals(p.getName())){
+                p.setName("选择正压");
+            }else if ("choiceNegative".equals(p.getName())){
+                p.setName("选择反压");
+            }else if ("rasterClose".equals(p.getName())){
+                p.setName("光栅屏蔽");
+            }else if ("safeDoorClose".equals(p.getName())){
+                p.setName("安全门屏蔽");
+            }else if ("buzzerClose".equals(p.getName())){
+                p.setName("蜂鸣器屏蔽");
+            };
+        });
+
+        return datas;
     }
 }
