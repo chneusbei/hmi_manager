bc678cc22996478f00525e903ff829fb85ace83a
zhengkai <1157377822@qq.com>
2020-04-23 18:24:02 +0800

    提交

--- src/main/java/com/plc/hmi/controler/zkcode/Plc4xCurveDataControler.java
+++ src/main/java/com/plc/hmi/controler/zkcode/Plc4xCurveDataControler.java
@@ -6,7 +6,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-
 import javax.annotation.Resource;
 import java.util.ArrayList;
 import java.util.List;
